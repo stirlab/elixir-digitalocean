0525670b4e64eeadb82f61b9531f8b4749cfb4a0
Chad Phillips <chad@apartmentlines.com>
2017-12-16 10:44:32 -0800

    fix default api endpoint

--- lib/digitalocean.ex
+++ lib/digitalocean.ex
@@ -4,11 +4,11 @@ defmodule DigitalOcean do
   Documentation for DigitalOcean.
   """
 
-  api_endpoint_default = "https://api.digitalocean.com/v2"
+  @api_endpoint_default "https://api.digitalocean.com/v2"
 
   use Tesla
 
-  plug Tesla.Middleware.BaseUrl, Application.get_env(:digitalocean, :api_endpoint, api_endpoint_default)
+  plug Tesla.Middleware.BaseUrl, Application.get_env(:digitalocean, :api_endpoint, @api_endpoint_default)
   plug Tesla.Middleware.Headers, %{"Authorization" => "Bearer " <> Application.get_env(:digitalocean, :access_token)}
   plug Tesla.Middleware.JSON
   if Application.get_env(:digitalocean, :debug_http) do
