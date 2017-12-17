b2675827ff7af09701af7d9d2d057dce2748bd50
Chad Phillips <chad@apartmentlines.com>
2017-12-16 20:03:35 -0800

    rip out delegation, not necessary

--- lib/digitalocean.ex
+++ lib/digitalocean.ex
@@ -6,7 +6,7 @@ defmodule DigitalOcean do
 
   @api_endpoint_default "https://api.digitalocean.com/v2"
 
-  use Tesla
+  use Tesla, only: [:head, :get, :post, :put, :delete]
 
   plug Tesla.Middleware.BaseUrl, Application.get_env(:digitalocean, :api_endpoint, @api_endpoint_default)
   plug Tesla.Middleware.Headers, %{"Authorization" => "Bearer " <> Application.get_env(:digitalocean, :access_token)}
@@ -14,20 +14,5 @@ defmodule DigitalOcean do
   if Application.get_env(:digitalocean, :debug_http) do
     plug Tesla.Middleware.DebugLogger
   end
-
-  defdelegate head(path), to: __MODULE__, as: :head
-  defdelegate head(path, opts), to: __MODULE__, as: :head
-
-  defdelegate get(path), to: __MODULE__, as: :get
-  defdelegate get(path, opts), to: __MODULE__, as: :get
-
-  defdelegate post(path), to: __MODULE__, as: :post
-  defdelegate post(path, opts), to: __MODULE__, as: :post
-
-  defdelegate put(path), to: __MODULE__, as: :put
-  defdelegate put(path, opts), to: __MODULE__, as: :put
-
-  defdelegate delete(path), to: __MODULE__, as: :delete
-  defdelegate delete(path, opts), to: __MODULE__, as: :delete
 
 end
