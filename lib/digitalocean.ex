858c4562dbd959fc554dad83a2b8d1e0c217cbdd
Chad Phillips <chad@apartmentlines.com>
2018-05-01 12:15:57 -0700

    fix compiled in app settings

--- lib/digitalocean.ex
+++ lib/digitalocean.ex
@@ -7,25 +7,18 @@ defmodule DigitalOcean do
   use Tesla, only: [:head, :get, :post, :put, :delete]
 
   @api_endpoint_default "https://api.digitalocean.com/v2"
-  @api_endpoint Application.get_env(:digitalocean_api_wrapper, :api_endpoint, @api_endpoint_default)
-
-  @http_follow_redirects Application.get_env(:digitalocean_api_wrapper, :http_follow_redirects, true)
-  @http_retry_enabled Application.get_env(:digitalocean_api_wrapper, :http_retry_enabled, true)
-  @http_retry_delay Application.get_env(:digitalocean_api_wrapper, :http_retry_delay, 1000)
-  @http_retry_max_retries Application.get_env(:digitalocean_api_wrapper, :http_retry_max_retries, 5)
-  @debug_http Application.get_env(:digitalocean_api_wrapper, :debug_http, false)
 
   plug Tesla.Middleware.Tuples, rescue_errors: :all
-  plug Tesla.Middleware.BaseUrl, @api_endpoint
+  plug Tesla.Middleware.BaseUrl, Application.get_env(:digitalocean_api_wrapper, :api_endpoint, @api_endpoint_default)
   plug Tesla.Middleware.Headers, %{"Authorization" => "Bearer " <> Application.fetch_env!(:digitalocean_api_wrapper, :access_token)}
   plug Tesla.Middleware.JSON
-  if @http_retry_enabled do
-    plug Tesla.Middleware.Retry, delay: @http_retry_delay, max_retries: @http_retry_max_retries
+  if Application.get_env(:digitalocean_api_wrapper, :http_retry_enabled, true) do
+    plug Tesla.Middleware.Retry, delay: Application.get_env(:digitalocean_api_wrapper, :http_retry_delay, 1000), max_retries: Application.get_env(:digitalocean_api_wrapper, :http_retry_max_retries, 5)
   end
-  if @http_follow_redirects do
+  if Application.get_env(:digitalocean_api_wrapper, :http_follow_redirects, true) do
     plug Tesla.Middleware.FollowRedirects
   end
-  if @debug_http do
+  if Application.get_env(:digitalocean_api_wrapper, :debug_http, false) do
     plug Tesla.Middleware.DebugLogger
   end
 
