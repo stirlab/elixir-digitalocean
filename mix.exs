57c16cad222835437738196b6277aae1bbea9518
Chad Phillips <chad@apartmentlines.com>
2018-10-26 19:51:35 -0700

    bump version, elixir version

--- mix.exs
+++ mix.exs
@@ -4,8 +4,8 @@ defmodule DigitalOcean.Mixfile do
   def project do
     [
       app: :digitalocean_api_wrapper,
-      version: "0.2.0",
-      elixir: "~> 1.6",
+      version: "0.2.1",
+      elixir: "~> 1.7",
       start_permanent: Mix.env == :prod,
       package: package(),
       description: description(),
